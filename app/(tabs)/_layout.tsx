1ee73586ef4267bcb0c0270a9bc8fc967d8f8543
Brandon Newton <114821633+BengalPirate@users.noreply.github.com>
2024-06-08 15:19:52 -0500

    updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { Tabs, Redirect } from 'expo-router';
+import { icons } from '../../constants';
+
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+        {name}
+      </Text>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
@@ -8,7 +26,15 @@ const TabsLayout = () => {
         <Tabs.Screen 
           name="home"
           options={{
-            title: ''
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon 
+                icon={icons.home}
+                name="Home"
+                focused={focused}
+              />
+            )
           }}
         />
       </Tabs>
