731a3651f5465c215a865bd9e1e27af7f7118a0c
Ethan Ricks <ethanwricks@gmail.com>
2024-06-12 13:14:45 -0500

    debugging app.test.js

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react-native';
-import App from '../app/app';
+import App from '../app/index';
 
 describe('<App />', () => {
   it('has a 1 title', () => {
     render(<App/>);
-    
+
   });
 });
