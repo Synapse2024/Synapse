6c5b0d3c960e08d343c20319f6c18bbc0636d30a
Brandon Newton <114821633+BengalPirate@users.noreply.github.com>
2024-06-12 22:22:49 -0500

    video bug

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,6 +1,8 @@
-import { View, Text, FlatList, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import React, { useState } from 'react';
+import { icons } from '@/constants';
+import { Video, ResizeMode, AVPlaybackStatus } from 'expo-av';
 
 interface TrendingItemProps {
   activeItem: Post;
@@ -12,13 +14,13 @@ const zoomIn = {
     transform: [{ scale: 0.9 }]
   },
   1: {
-    transform: [{ scale: 1 }]
+    transform: [{ scale: 1.1 }]
   }
 }
 
 const zoomOut = {
   0: {
-    transform: [{ scale: 1 }]
+    transform: [{ scale: 1.1 }]
   },
   1: {
     transform: [{ scale: 0.9 }]
@@ -28,6 +30,12 @@ const zoomOut = {
 const TrendingItem: React.FC<TrendingItemProps> = ({ activeItem, item }) => {
   const [play, setPlay] = useState(false);
 
+  const handlePlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (status.isLoaded && !status.isPlaying && status.didJustFinish) {
+      setPlay(false);
+    }
+  };
+
   return (
     <Animatable.View
       className="mr-5"
@@ -35,7 +43,14 @@ const TrendingItem: React.FC<TrendingItemProps> = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video 
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={handlePlaybackStatusUpdate}
+        />
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
@@ -49,6 +64,12 @@ const TrendingItem: React.FC<TrendingItemProps> = ({ activeItem, item }) => {
             className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
             resizeMode='cover'
           />
+
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode='contain'
+          />
         </TouchableOpacity>
       )}
     </Animatable.View>
@@ -60,6 +81,7 @@ interface Post {
   $id: string;
   id: number; 
   thumbnail: string; // Add other properties as needed
+  video: string; //? check this
 }
 
 // Define the props interface
@@ -68,7 +90,13 @@ interface TrendingProps {
 }
 
 const Trending: React.FC<TrendingProps> = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[0]);
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  const viewableItemsChanges = ({ viewableItems }: { viewableItems: Array<{ key: string }> }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key as unknown as Post); // Cast key to Post
+    }
+  }
 
   return (
     <FlatList 
@@ -77,6 +105,11 @@ const Trending: React.FC<TrendingProps> = ({ posts }) => {
         renderItem={({ item }) => (
             <TrendingItem activeItem={activeItem} item={item}/>
         )}
+        onViewableItemsChanged={viewableItemsChanges}
+        viewabilityConfig={{
+          itemVisiblePercentThreshold: 70
+        }}
+        contentOffset={{ x: 170, y: 0 }}
         horizontal
     />
   );
